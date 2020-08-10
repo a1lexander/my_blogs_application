41a5bdae56f3c20d94997e7f5270d668276360f1
Alexander <a1lehander1@gmail.com>
2020-08-10 09:02:55 +0300

    add security and docker-compose-prod.yml

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('universe-admin/', admin.site.urls),
 
     # User management
     path('accounts/', include('allauth.urls')),
